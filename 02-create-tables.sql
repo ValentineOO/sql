cfcd76a521edae3b24c0ad1eb17fc97891f1c0ed
ValentineOO <varlomxg@gmail.com>
2024-03-04 16:13:57 +0100

    create tables

--- 02-create-tables.sql
+++ 02-create-tables.sql
@@ -1,9 +1,11 @@
-CREATE TABLE events(
-    id,
-    name,
-    date_planned,
-    image,
-    description,
-    max_participants,
-    min_age
+CREATE TABLE events (
+    id INT PRIMARY KEY AUTO_INCREMENT, -- MySQL
+    -- id SERIAL PRIMARY KEY,
+    -- Postgres
+    name VARCHAR (300) NOT NULL CHECK (LENGTH(name) > 5),
+    date_planned TIMESTAMP NOT NULL,
+    image VARCHAR (300),
+    description TEXT NOT NULL,
+    max_participants INT CHECK (max_participants > 0),
+    min_age INT CHECK (min_age > 0)
 );
