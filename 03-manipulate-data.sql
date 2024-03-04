a0db9cfadf22795788746508f2619d44603fc305
ValentineOO <varlomxg@gmail.com>
2024-03-04 16:29:07 +0100

    manipulating data

---
+++ 03-manipulate-data.sql
@@ -0,0 +1,21 @@
+INSERT INTO events (
+        name,
+        date_planned,
+        description,
+        max_participants,
+        min_age
+    )
+VALUES (
+        'A first event',
+        '2020-02-29 14:00:00',
+        'This is the description of the first event.',
+        20,
+        18
+    ),
+    (
+        'A second event',
+        '2020-04-29 07:15:00',
+        'This is the description of the second event.',
+        20,
+        21
+    );
